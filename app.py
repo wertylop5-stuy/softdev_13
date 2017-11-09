20c7ad8232d8b57d74b55d77b8887282cc6fdb0b
Stanley Lin <slin55055@gmail.com>
2017-11-09 18:08:14 -0500

    finished

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 import json, urllib2
 
 app = Flask(__name__)
 
 def callApi(apiKey):
-	u = urllib2.open("key=%s"%(apiKey))
+	u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=%s"%(apiKey))
+	data = json.loads(u.read())
+	#data = u.read()
+	return data
+	
 
 @app.route("/")
 def root():
-	return "fef"
+	data = callApi("kLf6LgwimzxpO917Kpurrjpcsp1DkgThHaeayMsW")
+	#print data.keys()
+	#return data
+	
+	return render_template("stars.html", date=data["date"], title=data["title"],
+		image_url=data["url"], copyright=data["copyright"],
+		explanation=data["explanation"])
+	
 
 if __name__ == "__main__":
 	app.debug = True
 	app.run()
+
