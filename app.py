41eeb25ce28da604ae6447de6d226df610bb57ab
Stanley Lin <slin55055@gmail.com>
2017-11-10 14:44:27 -0500

    work

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 import json, urllib2
 
+app_id = 930378527100120
+
+
 app = Flask(__name__)
 
 def callApi(apiKey):
@@ -9,9 +12,15 @@ def callApi(apiKey):
 	#data = u.read()
 	return data
 	
+def getLoginLink(redirect_url):
+	return """
+https://www.facebook.com/
+v2.11/dialog/oauth?client_id=%s&redirect_uri=%s&
+response_type=token"""%(
+	str(app_id), redirect_url)
 
-@app.route("/")
-def root():
+@app.route("/nasa")
+def nasa():
 	data = callApi("kLf6LgwimzxpO917Kpurrjpcsp1DkgThHaeayMsW")
 	#print data.keys()
 	#return data
@@ -19,7 +28,13 @@ def root():
 	return render_template("stars.html", date=data["date"], title=data["title"],
 		image_url=data["url"], copyright=data["copyright"],
 		explanation=data["explanation"])
+
+
+@app.route("/")
+def root():
+	link = getLoginLink("https://localhost:5000")
 	
+	return render_template("fb.html", login_link=link)
 
 if __name__ == "__main__":
 	app.debug = True
